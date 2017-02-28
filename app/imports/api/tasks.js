68619c43ceeae71ccdea021831bef4559de9a25a
Zac <zknoebel@gmail.com>
2017-02-27 16:06:21 -1000

    Task 3: Collections, Forms and events, update and remove.

---
+++ app/imports/api/tasks.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zac on 2/27/17.
+ */
+
+
+
+
+
+import { Mongo } from 'meteor/mongo';
+
+
+
+export const Tasks = new Mongo.Collection('tasks');
+
--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -2,15 +2,49 @@
  * Created by zac on 2/25/17.
  */
 
+
+
 import { Template } from 'meteor/templating';
+
+
+
+import { Tasks } from '../api/tasks.js';
+
+
+
+import './task.js';
 
 import './body.html';
 
+
+
 Template.body.helpers({
-  tasks:  [
-    { text: 'This is task 1'},
-    { text: 'This is task 2'},
-    { text: 'This is task 3'}
-  ],
-    }
-);
+  tasks() {
+
+    return Tasks.find({});
+
+  },
+
+});
+
+Template.body.events({
+  'submit .new-task'(event){
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    target.text.value = '';
+  }
+})
+
+Template.body.helpers({
+  tasks(){
+    return Tasks.find({}, { sort: { createdAt: -1 }});
+  },
+});
